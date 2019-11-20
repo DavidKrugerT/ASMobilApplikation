ee8bb9276c7b84d6190231654b811a37c22b5ccf
David Krüger <david.tropakruger@gmail.com>
2019-11-20 23:53:19 +0100

    We can now open and close a table, next task is to add the item list

--- app/src/main/java/com/example/antonsskafferiappliaktion/MainActivity.java
+++ app/src/main/java/com/example/antonsskafferiappliaktion/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.antonsskafferiappliaktion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Dialog takeOrders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        takeOrders = new Dialog(this);
+    }
+
+    public void ShowPopup(View view) {
+        TextView txtclose;
+        takeOrders.setContentView(R.layout.custompopup);
+        txtclose = takeOrders.findViewById(R.id.txtclose);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeOrders.dismiss();
+            }
+        });
+        takeOrders.show();
     }
 }
