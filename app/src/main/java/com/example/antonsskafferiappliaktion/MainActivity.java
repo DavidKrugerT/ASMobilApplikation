80a07634503a0469e9fe4d036c3a725157239d08
Jonathan Manousian <Joma1705@student.miun.se>
2019-11-21 16:53:50 +0100

    schedule-xml + class + button on Main->schedule

--- app/src/main/java/com/example/antonsskafferiappliaktion/MainActivity.java
+++ app/src/main/java/com/example/antonsskafferiappliaktion/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.antonsskafferiappliaktion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         takeOrders = new Dialog(this);
+
+        //Button button2schedule;
+
+         Button button2schedule = (Button) findViewById(R.id.button2schedule);
+        button2schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, schduleActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void ShowPopup(View view) {
