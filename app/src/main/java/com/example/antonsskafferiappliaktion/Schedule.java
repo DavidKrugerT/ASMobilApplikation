934dabe92c012533fec5be954c06469cda749cd2
Jonathan Manousian <Joma1705@student.miun.se>
2019-11-23 20:40:41 +0100

    Dropdown och Checkboxes i schemat

---
+++ app/src/main/java/com/example/antonsskafferiappliaktion/Schedule.java
@@ -0,0 +1,34 @@
+package com.example.antonsskafferiappliaktion;
+
+public class Schedule {
+
+    private String name;
+    private String tid;
+    private String datum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+
+}
--- app/src/main/java/com/example/antonsskafferiappliaktion/schduleActivity.java
+++ app/src/main/java/com/example/antonsskafferiappliaktion/schduleActivity.java
@@ -1,21 +1,26 @@
 package com.example.antonsskafferiappliaktion;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class schduleActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class schduleActivity extends AppCompatActivity implements OnItemSelectedListener {
 
     CalendarView calendarView;
     TextView myDate;
@@ -24,55 +29,98 @@ public class schduleActivity extends AppCompatActivity {
     TextView sparadTid;
     TextView sparadDatum;
 
-    String name, tid, datum;
+    Schedule schedule = new Schedule();
 
     EditText nameInput;
 
-    EditText tidInput;
-
     Button saveButton;
+
+    CheckBox lunch;
+    CheckBox kvell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schdule);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.names, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+
         calendarView = (CalendarView) findViewById(R.id.calendarView);
         myDate = (TextView) findViewById(R.id.myDate);
-        nameInput = (EditText) findViewById(R.id.nameInput);
-        tidInput = (EditText) findViewById(R.id.tidInput);
+
         saveButton = (Button) findViewById(R.id.saveButton);
 
         sparadNamn = (TextView) findViewById(R.id.sparadNamn);
         sparadTid = (TextView) findViewById(R.id.sparadTid);
         sparadDatum = (TextView) findViewById(R.id.sparadDatum);
 
+        lunch = (CheckBox) findViewById(R.id.lunch);
+        kvell = (CheckBox) findViewById(R.id.kvell);
+
+        lunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                schedule.setTid("Lunch");
+            }
+        });
+
+        kvell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                schedule.setTid("Kväll");
+            }
+        });
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = year + "/" + month + "/" + dayOfMonth;
                 myDate.setText(date);
-                datum = date;
+                schedule.setDatum(date);
             }
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                name = nameInput.getText().toString();
-                tid = tidInput.getText().toString();
 
-                sparadDatum.setText(datum);
-
-
-
-                sparadNamn.setText(name);
-
-                sparadTid.setText(tid);
+                sparadDatum.setText(schedule.getDatum());
+                sparadNamn.setText(schedule.getName());
+                sparadTid.setText(schedule.getTid());
 
             }
         });
 
     }
 
+    /*private class GetPerson extends AsyncTask<Void, Void, Void> {
+        private String apiUrl = "";
+        private String strXml;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }*/
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        schedule.setName(parent.getItemAtPosition(position).toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
